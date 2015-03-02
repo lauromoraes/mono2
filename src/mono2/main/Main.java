1af14ab09262529f6c5d9c0be866078669f8a574
Lauro Moraes <lauro_moraes@hotmail.com>
2015-03-02 14:30:40 -0300

    Alter Main
    Change-Id: I627082d1e0c1e4a42b4ae801d3ec1dfe81cd75c5
    Signed-off-by: Lauro Moraes <lauro_moraes@hotmail.com>

--- src/mono2/main/Main.java
+++ src/mono2/main/Main.java
@@ -1,9 +1,25 @@
 package mono2.main;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import mono2.table.Table;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String path = "/home/lauro/Documents/eclipseWorkspace/mono2/src/mono2/input/input_paper.txt";
+		Set<Integer> rowSet = new HashSet<Integer>();
+		rowSet.add(1);
+		rowSet.add(2);
+		rowSet.add(3);
+		rowSet.add(4);
+		rowSet.add(5);
+		Set<Integer> xRowSet = new HashSet<Integer>();
+		
+		Table table = new Table();
+		table.readTable(path);
+		table.enumerationTree(rowSet, xRowSet, rowSet.size()+1);
 	}
 
 }
